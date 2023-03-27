c0013de31638a920005a1de1c0ccd42d5be8f0c8
yoslie <65396086+yoslie@users.noreply.github.com>
2023-03-27 10:48:00 +0700

    style: fix bug style on navbar

--- components/Navbar/Mobile/List/Item/Children/index.vue
+++ components/Navbar/Mobile/List/Item/Children/index.vue
@@ -3,10 +3,10 @@
       <li v-for="(menu, index) in menus" :key="index">
         <a :href="menu.link" :target="menu.link.startsWith('http') ? '_blank' : ''">
           <div @click="$emit('click')" class="flex flex-col p-2 gap-2">
-            <h1 class="font-roboto text-[#212121] text-[15px] font-medium">
+            <h1 class="!text-[#212121] !text-[15px] !font-roboto !font-medium !leading-[26px]">
               {{ menu.title }}
             </h1>
-            <p class="text-[#616161] text-sm line-clamp-2">
+            <p class="!text-[#616161] !text-sm !font-lato !font-light !leading-[23px] line-clamp-2">
               {{ menu.description }}
             </p>
           </div>
