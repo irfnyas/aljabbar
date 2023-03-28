d5a504d2017c0ef0001b7b9000cd1796f3a7c6dc
yoslie <65396086+yoslie@users.noreply.github.com>
2023-03-28 10:59:46 +0700

    style: fix max width not worked

--- components/Blog/Title/index.vue
+++ components/Blog/Title/index.vue
@@ -15,7 +15,7 @@
         {{ title }}
       </h1>
       <div class="w-32 border-2 border-green-600" />
-      <p v-html="subtitle" class="text-gray-700 font-light text-sm md:text-base lg:text-center max-w-[832px]leading-[23px] md:leading-[26px]" />
+      <p v-html="subtitle" class="text-gray-700 font-light text-sm md:text-base lg:text-center max-w-[840px] leading-[23px] md:leading-[26px]" />
     </div>
     <slot />
   </div>
